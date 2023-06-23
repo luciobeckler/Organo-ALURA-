1abb2486dc29ff5b61ec206918763335b8f3608b
Lucio_Beckler <luciobecklerp@gmail.com>
2023-06-23 10:19:26 -0300

    Criação do projeto e Banner finalizados, Form em progresso.

---
+++ src/components/CampoTexto/CampoTexto.js
@@ -0,0 +1,12 @@
+import './CampoTexto.css'
+
+export const CampoTexto = (props) => {
+    return (
+        <div className="campo-texto">
+            <label>
+                {props.label}
+            </label>
+            <input placeholder={props.placeholder} />
+        </div>
+    )
+}
