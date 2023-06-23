aeeb0bbbf6cd2486d4ac41a944298c8b17c1a448
Lucio_Beckler <luciobecklerp@gmail.com>
2023-06-23 11:48:34 -0300

    Estilo da Lista Suspensa implementado

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,6 +1,17 @@
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 const Formulario = () => {
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Deveops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
     return (
         <section className='formulario'>
             <form>
@@ -8,6 +19,7 @@ const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite seu nome" />
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa label="Time" itens={times} />
             </form>
         </section>
     )
