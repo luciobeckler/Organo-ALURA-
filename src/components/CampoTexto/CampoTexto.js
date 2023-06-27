7687c428a4b576a95f4f01b0b8d5cb71fd5f4717
Lucio_Beckler <luciobecklerp@gmail.com>
2023-06-27 10:02:15 -0300

    Controlando estados e os capturando usando hooks com useState

--- src/components/CampoTexto/CampoTexto.js
+++ src/components/CampoTexto/CampoTexto.js
@@ -2,17 +2,18 @@ import './CampoTexto.css'
 
 export const CampoTexto = (props) => {
 
-    let valor = 'Lúcio Beckler'
+
+    //Hook usado para previnir o problema de travamento ao definir um nome inicial
     const aoDigitado = (event) => {
-        valor = event.target.value
-        console.log(valor)
+        props.aoAlterado(event.target.value)
+        //console.log(valor)
     }
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} holder={props.placeholder} /> {/* Passado o obrigatorio como parâmetro ao chamar campo texto e validando dentro do component */}
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} holder={props.placeholder} /> {/* Passado o obrigatorio como parâmetro ao chamar campo texto e validando dentro do component */}
         </div>
     )
 }
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,6 +1,7 @@
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import Botao from '../Botao'
+import React, { useState } from 'react';
 
 import './Formulario.css'
 const Formulario = () => {
@@ -16,17 +17,45 @@ const Formulario = () => {
 
     const aoSalvar = (evento) => {
         evento.preventDefault()
-        console.log("Form foi submetido")
+        console.log("Form foi submetido =>", nome, cargo, imagem, time)
     }
+
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
 
     return (
         <section className='formulario'>
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
-                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
-                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-                <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Nome"
+                    placeholder="Digite seu nome"
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Cargo"
+                    placeholder="Digite seu cargo"
+                    valor={cargo}
+                    aoAlterado={cargo => setCargo(cargo)}
+                />
+                <CampoTexto
+                    label="Imagem"
+                    placeholder="Digite o endereço da imagem"
+                    valor={imagem}
+                    aoAlterado={imagem => setImagem(imagem)}
+                />
+                <ListaSuspensa
+                    obrigatorio={true}
+                    label="Time"
+                    itens={times}
+                    valor={times}
+                    aoAlterado={time => setTime(time)}
+                />
                 <Botao>Criar Card</Botao>
             </form>
         </section>
