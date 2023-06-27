b1410a4141ade4ba7feab9c4a9ace84e24f4c338
Lucio_Beckler <luciobecklerp@gmail.com>
2023-06-27 08:34:56 -0300

    Capturando uma variável

--- src/components/CampoTexto/CampoTexto.js
+++ src/components/CampoTexto/CampoTexto.js
@@ -1,12 +1,18 @@
 import './CampoTexto.css'
 
 export const CampoTexto = (props) => {
+
+    let valor = 'Lúcio Beckler'
+    const aoDigitado = (event) => {
+        valor = event.target.value
+        console.log(valor)
+    }
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input required={props.obrigatorio} holder={props.placeholder} /> {/* Passado o obrigatorio como parâmetro ao chamar campo texto e validando dentro do component */}
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} holder={props.placeholder} /> {/* Passado o obrigatorio como parâmetro ao chamar campo texto e validando dentro do component */}
         </div>
     )
 }
