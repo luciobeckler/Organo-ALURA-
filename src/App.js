162edd182967c4e64d6ec1a888c4eacc080fc63f
Lucio_Beckler <luciobecklerp@gmail.com>
2023-06-29 09:06:17 -0300

    Começando a estilizar os times

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './components/Banner';
 import Formulario from './components/Formulario';
+import Time from './components/TIme';
 
 function App() {
 
@@ -14,6 +15,10 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoColaboradorCadastrado={aoNovoColaboradorAdicionado}></Formulario>
+      <Time nome="Programação"></Time>
+      <Time nome="FrontEnd"></Time>
+      <Time nome="Data Science"></Time>
+
     </div>
   );
 }
