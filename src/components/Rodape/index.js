8d55af44cf8ad0fec261990b11515a51b9f29ca7
Lucio_Beckler <luciobecklerp@gmail.com>
2023-07-01 16:07:48 -0300

    Footer finalizado

--- src/components/Rodape/index.js
+++ src/components/Rodape/index.js
@@ -8,7 +8,21 @@ const Rodape = () => {
                     <li>
                         <img src='/imagens/imagens/fb.png' />
                     </li>
+                    <li>
+                        <img src='./imagens/imagens/tw.png' />
+                    </li>
+                    <li>
+                        <img src='./imagens/imagens/ig.png' />
+                    </li>
                 </ul>
+            </section>
+
+            <section>
+                <img src='/imagens/imagens/logo.png' />
+            </section>
+
+            <section>
+                <h3>Desenvolvido por Lúcio Beckler em parceria com Alura!</h3>
             </section>
         </footer>
     )
