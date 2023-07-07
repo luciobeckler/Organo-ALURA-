ce8c3a5e4e97359fe5814353065b895336d07377
Lucio_Beckler <luciobecklerp@gmail.com>
2023-07-07 11:39:40 -0300

    Adicionando feature de delete de colaboradores e adicionando ID's únicos para os objetos nas listas

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,7 +1,13 @@
 import './colaborador.css'
+import { AiFillCloseCircle } from 'react-icons/ai';
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
     return (<div className="colaborador">
+        <AiFillCloseCircle
+            size={25}
+            className="deletar"
+            onClick={() => aoDeletar(colaborador.id)}
+        />
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
